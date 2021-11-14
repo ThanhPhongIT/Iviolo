2d262772f38453431f14ff36384857da60ecbbcb
ThanhPhong <phonga3bdq@gmail.com>
2021-11-14 16:22:09 +0700

    merge

--- src/js/main.js
+++ src/js/main.js
@@ -1,50 +1,52 @@
-
-$(document).ready(function(){
-  $('.variable-width').slick({
+$(document).ready(function () {
+  $(".variable-width").slick({
     dots: false,
     infinite: true,
     arrows: true,
     speed: 300,
     slidesToShow: 1,
     variableWidth: true,
-    prevArrow: '<button class="slide-arrow slick-prev"><img src="images/icon-carousel-prev.png"></button>',
-    nextArrow: '<button class="slide-arrow slick-next"><img src="images/icon-carousel-next.png"></button>'
+    prevArrow:
+      '<button class="slide-arrow slick-prev"><img src="images/icon-carousel-prev.png"></button>',
+    nextArrow:
+      '<button class="slide-arrow slick-next"><img src="images/icon-carousel-next.png"></button>',
   });
 
-
-  $('.slider-for').slick({
+  $(".slider-for").slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: true,
     fade: false,
-    asNavFor: '.slider-nav',
-    lazyLoad: 'ondemand',
-    prevArrow: '<button class="slide-arrow slick-prev"><i class="fas fa-chevron-left"></i></button>',
-    nextArrow: '<button class="slide-arrow slick-next"><i class="fas fa-chevron-right"></i></button>'
+    asNavFor: ".slider-nav",
+    lazyLoad: "ondemand",
+    prevArrow:
+      '<button class="slide-arrow slick-prev"><i class="fas fa-chevron-left"></i></button>',
+    nextArrow:
+      '<button class="slide-arrow slick-next"><i class="fas fa-chevron-right"></i></button>',
   });
-	$('.slider-x').slick({
+  $(".slider-x").slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: true,
     fade: false,
-    asNavFor: '.slider-nav',
-    lazyLoad: 'ondemand',
-    dots: false
+    asNavFor: ".slider-nav",
+    lazyLoad: "ondemand",
+    dots: false,
   });
-  $('.slider-nav').slick({
+  $(".slider-nav").slick({
     slidesToShow: 4,
     slidesToScroll: 1,
-    asNavFor: '.slider-for',
+    asNavFor: ".slider-for",
     dots: false,
     focusOnSelect: true,
-    lazyLoad: 'ondemand',
-    prevArrow: '<button class="slide-arrow slick-prev"><i class="fas fa-chevron-left"></i></button>',
-    nextArrow: '<button class="slide-arrow slick-next"><i class="fas fa-chevron-right"></i></button>'
-  });          
-
-
-  $(".click-search").click(function(){
-    $(this).addClass("active");
+    lazyLoad: "ondemand",
+    prevArrow:
+      '<button class="slide-arrow slick-prev"><i class="fas fa-chevron-left"></i></button>',
+    nextArrow:
+      '<button class="slide-arrow slick-next"><i class="fas fa-chevron-right"></i></button>',
   });
 
+  $(".click-search").click(function () {
+    $(this).addClass("active");
+  });
 });
